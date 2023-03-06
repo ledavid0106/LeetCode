7334a1c09398ef5efd84e3ca43661f89ae8b62dc
David Le <104813594+ledavid0106@users.noreply.github.com>
2023-03-05 19:41:14 -0800

    Time: 19 ms (67.00%), Space: 13.5 MB (87.34%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+                stack.append(char)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
+            else:
+                return False
+        return stack == []
