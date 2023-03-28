66e803b2c1876f1ade876015c1b19f5d54812a8e
David Le <104813594+ledavid0106@users.noreply.github.com>
2023-03-27 23:57:15 -0700

    Time: 18 ms (54.12%), Space: 13.8 MB (32.72%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        end = len(s) - 1
+        while end > 0 and s[end] == " ": end -= 1
+        beg = end
+        while beg >= 0 and s[beg] != " ": beg -= 1
+        return end - beg
